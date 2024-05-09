db4d5955978651a3fe40601a96a0582fb1d722b1
Sebastian Leiva <seba@Sebastians-MacBook-Air.local>
2024-05-09 14:18:39 -0400

    Set new path output

--- app/main.py
+++ app/main.py
@@ -8,5 +8,5 @@ PROCESS_WEEKS = 3
 if __name__ == '__main__':
     data_creator = DataCreator(PRINTS_PATH, TAPS_PATH, PAYS_PATH, PROCESS_WEEKS)
     dataset = data_creator.get_output_dataset()
-    dataset.to_csv('output.csv', index=False)
+    dataset.to_csv('./app/output.csv', index=False)
     print(dataset)
